2cb9efdc353bf3f2f9b9a3fd58d9cab1ca8d1eb2
Samuel <samuel.bitdinger@gmail.com>
2024-10-17 14:48:39 -0300

    animacao de digitacao no titulo

--- src/components/main/about.tsx
+++ src/components/main/about.tsx
@@ -1,9 +1,16 @@
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 function About(){
+    const [title] = useTypewriter({
+        words: ["Front-end dev"],
+        loop: Infinity,
+    })
+
+
     return(
         <section className="grid about-columns justify-between gap-12 mt-16">
             <div className="text-neutral-200">
-                <h1 className="text-7xl font-bold mb-8">Front-end dev</h1>
+                <h1 className="text-7xl font-bold mb-8">{title} <Cursor cursorColor="#b91c1c"/></h1>
                 <p className="text-2xl font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam distinctio mollitia voluptatibus officiis provident voluptate cupiditate sint quia, autem, deleniti molestias iure eligendi. Minus soluta aliquid odio saepe, labore nihil.</p>
             </div>
             <img src="../../../img/foto.png" alt="Foto" className="w-72"/>
