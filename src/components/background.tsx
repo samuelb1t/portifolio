3b5e99572c23c1d4f39edb4cbfc23ebba97b9ec3
Samuel <samuel.bitdinger@gmail.com>
2024-10-25 10:52:46 -0300

    bugFix

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,9 +1,9 @@
-import { AnimatedBackground } from 'animated-backgrounds';
+// import { AnimatedBackground } from 'animated-backgrounds';
 
 function bg({ children,id }: { children: React.ReactElement,id:string }) {
   return (
-    <article className=" h-screen px-60 py-8" id={id}>
-        <AnimatedBackground animationName="fireflies"/>
+    <article className="bg h-screen px-60 py-8" id={id}>
+        {/* <AnimatedBackground animationName="fireflies"/> */}
         <section className="bg-neutral-900 w-full h-full block rounded-3xl display-font text-3xl pt-12 pb-16 px-16">
           {children}
         </section>
