c4a338b9cb34346905c5dfdca2aba6cd3a3c3bb1
Samuel <samuel.bitdinger@gmail.com>
2024-11-14 20:58:40 -0300

    bugfix

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,6 +1,7 @@
 import Menu from "./menu-ham";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import logo from "../../../img/samuel.svg";
 
 function Header({menuId}:{menuId:string}) {
   const [active,setActive] = useState(true);
@@ -11,7 +12,7 @@ function Header({menuId}:{menuId:string}) {
 }
   return (
     <header className="flex justify-between mb-12 relative">
-      <a onClick={() => {navigate("/"); }}><img src="../../../img/samuel.svg" alt="'Logo'" className="mt-4 cursor-pointer"/></a>
+      <a onClick={() => {navigate("/"); }}><img src={logo} className="mt-4 cursor-pointer"/></a>
       <div>
         <Menu isVisible={active} toggleVisibility={toggleMenu} id={menuId}></Menu>
       </div>
