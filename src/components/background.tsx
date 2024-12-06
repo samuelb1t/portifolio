347a492866a3ee7d9f6cfa3d4a75d01bbda713e7
Samuel <samuel.bitdinger@gmail.com>
2024-12-06 15:52:40 -0300

    bug fix responsividade

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,10 +1,10 @@
 
 function bg({ children,id }: { children: React.ReactElement,id:string }) {
   return (
-    <article className="bg h-screen px-4 md:px-8 xl:px-60 py-12 lg:py-8" id={id}>
-        <section className="bg-neutral-900 w-full h-full block rounded-3xl display-font text-3xl pt-12 2xl:pt-36 sm:pb-24 md:pb-20 2xl:pb-48 px-0 xl:px-12">
+    <article className="bg h-screen px-4 md:px-8 xl:px-60 py-16 lg:py-8" id={id}>
+        <section className="bg-neutral-900 w-full h-full block rounded-3xl display-font text-3xl pt-8 2xl:pt-36 pb-16 md:pb-20 2xl:pb-48 px-0 xl:px-12">
           {children}
-        </section>
+        </section>  
       </article>
   );
 }
